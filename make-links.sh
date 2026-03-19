10eac4a6a4e9cc766984b4ed017f4a00ffa7c4d3
Liam Pribis <liam.pribis@digiconsoles.com>
2026-03-20 10:30:34 +1300

    Add tmux and inputrc

--- make-links.sh
+++ make-links.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
-set -eux
+set -eu
 
-GC="$HOME/.gitconfig"
+link_file() {
+    local src="$1" dst="$2"
+    if [ -f "$dst" ]; then
+        echo "$dst already exists"
+    else
+        ln -s "$src" "$dst"
+    fi
+}
+
 THISDIR="$(cd "$(dirname "$0")" && pwd)"
 
-if [ -f "$GC" ]; then
-    echo "$GC already exists"
-    exit 1
-fi
-
-ln -s "$THISDIR/.gitconfig" "$GC"
-
+link_file "$THISDIR/.gitconfig" "$HOME/.gitconfig"
+link_file "$THISDIR/.tmux.conf" "$HOME/.tmux.conf"
+link_file "$THISDIR/.inputrc"   "$HOME/.inputrc"
