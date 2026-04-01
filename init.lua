199dc83fa603c924e0c76d6996d1f59d2a5443d7
Liam Pribis <liam.pribis@digiconsoles.com>
2026-04-02 11:06:47 +1300

    More changes for completion and tweak

--- init.lua
+++ init.lua
@@ -15,16 +15,58 @@ vim.opt.smartcase = true
 vim.opt.showmatch = true
 vim.opt.confirm = true
 
-vim.keymap.set("n", ")", "<cmd>cnext<cr>", {noremap = true})
-vim.keymap.set("n", "(", "<cmd>cprev<cr>", {noremap = true})
-vim.keymap.set("n", "<Leader>h", "<cmd>nohlsearch<cr><cmd>lclose<cr><cmd>cclose<cr>")
-vim.keymap.set("n", "<Bslash>", "<C-^>", {silent = true})
+vim.keymap.set("n", ")", "<cmd>cnext<cr>",
+    { noremap = true, desc = "next quickfix item" })
+vim.keymap.set("n", "(", "<cmd>cprev<cr>",
+    { noremap = true, desc = "prev quickfix item" })
+
+vim.keymap.set("n", "<Leader>h", "<cmd>nohlsearch<cr><cmd>lclose<cr><cmd>cclose<cr>",
+    { desc = "close all search things" })
+
+vim.keymap.set("n", "<Bslash>", "<C-^>",
+    { silent = true, desc = "swap to previous file" })
 
 vim.opt.clipboard = "unnamedplus"
 vim.opt.completeopt = "fuzzy,menu,popup"
--- redirect c-n completion to always be omnicomplete
-vim.keymap.set("i", "<C-n>", "<C-x><C-o>", { noremap = true })
-vim.keymap.set("i", "<C-p>", "<C-x><C-o>", { noremap = true })
+
+local function try_omni_then_keyword_complete(kw_key)
+  if vim.bo.omnifunc ~= "" then
+      return "<C-x><C-o>"
+  else
+      return kw_key
+  end
+end
+
+local function toggle_omni()
+  if vim.bo.omnifunc ~= "" then
+    vim.b._saved_omnifunc = vim.bo.omnifunc
+    vim.bo.omnifunc = ""
+    vim.notify("Omnicomplete disabled")
+  else
+    vim.bo.omnifunc = vim.b._saved_omnifunc or ""
+    vim.notify("Omnicomplete enabled: " .. vim.bo.omnifunc)
+  end
+end
+
+-- redirect c-n/c-p should be omni-complete if it exists, otherwise keyword complete
+vim.keymap.set("i", "<C-n>", function() return try_omni_then_keyword_complete("<C-n>") end,
+    { expr = true, noremap = true, desc = "omni or kewword complete" })
+vim.keymap.set("i", "<C-p>", function() return try_omni_then_keyword_complete("<C-p>") end,
+    { expr = true, noremap = true, desc = "omni or kewword complete" })
+
+-- <L>o toggles omnicomplete
+vim.keymap.set("n", "<leader>o", toggle_omni,
+    { desc = "toggle omnicomplete" })
+
+-- grepping
+vim.opt.grepprg = "rg --vimgrep" -- by default this uses -uu flag which ignores gitignore
+-- <L>s greps for word under cursor
+vim.keymap.set("n", "<Leader>s", "\"jyiw:grep! <C-r><C-w><cr><cmd>cope<cr>",
+    { silent = true, desc = "quickfix search word under cursor" })
+
+-- <L>e toggles diagnostics
+vim.keymap.set( "n", "<Leader>e", function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end,
+    { silent = true, noremap = true, desc = "toggle diagnostics" })
 
 vim.pack.add( {
     { src = "https://github.com/unblevable/quick-scope", version = "v2.7.1", },
@@ -45,9 +87,12 @@ require('mini.pick').setup({
         choose_marked = '<C-q>',
     }
 })
-vim.keymap.set("n", "<C-p>", MiniPick.builtin.files)
-vim.keymap.set("n", "<C-k>", function() MiniPick.builtin.files({tool = "fallback"}) end)
-vim.keymap.set("n", "<Leader>g", MiniPick.builtin.grep_live)
+vim.keymap.set("n", "<C-p>", MiniPick.builtin.files,
+    { desc = "pick files" })
+vim.keymap.set("n", "<C-k>", function() MiniPick.builtin.files({tool = "fallback"}) end,
+    { desc = "pick files (dumb)" })
+vim.keymap.set("n", "<Leader>g", MiniPick.builtin.grep_live,
+    { desc = "live grep" })
 
 -- OIL
 require("oil").setup({
@@ -71,10 +116,12 @@ require("oil").setup({
         show_hidden = true,
     },
 })
-vim.keymap.set("n", "<Leader>1", "<cmd>Oil<CR>")
+vim.keymap.set("n", "<Leader>1", "<cmd>Oil<CR>",
+    { desc = "open file browser" })
 
 -- PARINFER
-vim.keymap.set("n", "<Leader>pi", "<cmd>ParinferToggle<CR>")
+vim.keymap.set("n", "<Leader>pi", "<cmd>ParinferToggle<CR>",
+    { desc = "toggle parinfer" })
 
 -- VLIME
 vim.g.vlime_compiler_policy = { DEBUG = 3 }
